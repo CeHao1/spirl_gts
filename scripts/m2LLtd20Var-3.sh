be99361416e1dcf907f6d2516136ae4ff4464b6b
cehao <cehao@outlook.com>
2023-05-05 02:43:49 -0700

    push

--- scripts/m2LLtd20Var-3.sh
+++ scripts/m2LLtd20Var-3.sh
@@ -1,6 +1,6 @@
 python3 spirl/rl/train.py --path=spirl/configs/hrl/maze/shLL_m2  --gpu=0 \
 --resume='latest' --resume_load_replay_buffer=0 --strict_weight_loading=0 \
---prefix=LL_td20_Var-3_s0_01 --seed=0
+--prefix=LL_td20_Var-3_s0_02 --seed=0
 
 python3 spirl/rl/train.py --path=spirl/configs/hrl/maze/shLL_m2  --gpu=0 \
 --resume='latest' --resume_load_replay_buffer=0 --strict_weight_loading=0 \
